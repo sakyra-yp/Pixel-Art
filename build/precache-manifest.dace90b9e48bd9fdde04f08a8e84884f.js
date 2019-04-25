9f72712f99ac645b038923c46d95760b0ba939cb
sakyra <ipodsasha0393@gmail.com>
2019-04-25 23:01:39 +0300

    build

---
+++ build/precache-manifest.dace90b9e48bd9fdde04f08a8e84884f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "0904ca86759c79786f43",
+    "url": "/static/js/main.82991f26.chunk.js"
+  },
+  {
+    "revision": "c96e88f077c5841a5a64",
+    "url": "/static/js/2.a4cc1517.chunk.js"
+  },
+  {
+    "revision": "0904ca86759c79786f43",
+    "url": "/static/css/main.459e779b.chunk.css"
+  },
+  {
+    "revision": "261ad28fa9905d276f62a0e44e804978",
+    "url": "/index.html"
+  }
+];
